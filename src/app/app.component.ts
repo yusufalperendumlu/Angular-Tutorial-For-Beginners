bb79f8a16b25d623649d27cd55d378804d87961d
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-07-28 08:46:07 +0300

    angular tutorial

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string;
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  togglePokemon: boolean = false;
+
+  constructor() {
+    this.title = 'Alperen';
+  }
+}
