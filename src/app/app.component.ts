8756c156261a976f5c21fde4b8f81dd4de79e306
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-07-28 08:53:46 +0300

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string;
-  numberOne: number = 1;
-  numberTwo: number = 2;
-  togglePokemon: boolean = false;
+  // title: string;
+  // numberOne: number = 1;
+  // numberTwo: number = 2;
+  // togglePokemon: boolean = false;
+
+  // constructor() {
+  //   this.title = 'Alperen';
+  // }
+
+  title: string = "Basket of baby beavers";
+  imgSrc: string = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nba.com%2F&psig=AOvVaw0p5ihR89gNrafpK1MBJ3El&ust=1690609814721000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPD2073asIADFQAAAAAdAAAAABAE"
 
   constructor() {
-    this.title = 'Alperen';
+    console.log("Constructor called");
   }
 }
