3cef8b94ec3005cd51937b73a3ca1a3ae822f538
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-07-30 02:48:36 +0300

    Components In-Depth & ngOnInit

--- src/app/user-base/user-list/user-list.component.ts
+++ src/app/user-base/user-list/user-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit{
+  user: string;
 
+  constructor() {
+    this.user = "";
+  }
+
+  ngOnInit(): void {
+    console.log('UserListComponent ngOnInit()');
+    this.user = "";
+  }
 }
+
+let testExample = 1;
